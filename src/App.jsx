9a1a9e368370a483ed55460b2cb87f1276955b0d
sam-karapetyan <karapetyans317@gmail.com>
2026-07-28 22:44:39 +0400

    verj erkrod main demq scaner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Nkarker from './Components/AnhatMain/1Nkarker';
+import NkarDemq from './Components/AnhatMain/2Nkardemq'; // 1. Ավելացվել է import-ը
 
 // Փորձնական երկրորդ էջ (կարող ես հետագայում սարքել առանձին ֆայլով)
 function Deposits() {
@@ -20,7 +21,15 @@ function App() {
         <Header />
         <Routes>
           {/* Գլխավոր էջ */}
-          <Route path="/" element={<Nkarker />} />
+          <Route 
+            path="/" 
+            element={
+              <>
+                <Nkarker />
+                <NkarDemq /> {/* 2. Ավելացվել է այստեղ */}
+              </>
+            } 
+          />
 
           {/* Երկրորդ էջ (օրինակ՝ /deposits) */}
           <Route path="/deposits" element={<Deposits />} />
